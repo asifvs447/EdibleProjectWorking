3c19cddd4cd1813d77745f6554f0df55bbb4fea7
Palansh Agarwal <palanshagarwal@gmail.com>
2018-04-22 13:47:47 +0530

    change DB config to mysql

--- ediblearrangements/settings.py
+++ ediblearrangements/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'ediblearrangements.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edible',
+        'USER': 'edible_admin',
+        'PASSWORD': 'ediblearrangements',
+        'HOST': 'edible.crf1srrole7i.us-west-1.rds.amazonaws.com',
+        'PORT': 3306,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
